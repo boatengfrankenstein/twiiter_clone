892644d748d701ffcfb17c402e864cb91b4607a9
daniel <boatengfrankenstein@gmail.com>
2019-02-26 11:27:23 -0500

    Twitter clone with react and rails

---
+++ app/javascript/components/main.jsx
@@ -0,0 +1,16 @@
+class Main extends React.Component {
+    render() {
+        return (
+            <h1> Hello from the component </h1>
+        );
+    }
+}
+
+/* let documentReady = () => {
+    React.render(
+    <Main />,
+     document.getElementById('react')
+    );
+};
+
+$(documentReady);  */
